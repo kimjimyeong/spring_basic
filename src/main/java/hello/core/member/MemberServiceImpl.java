450eac29bfc5f7c43e65a6d9a4093dbb394d7007
김지명 <ehxhfl1589@gmail.com>
2023-04-06 01:08:17 +0900

    test

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,21 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+
+    // interface만 존재
+    private final MemberRepository memberRepository;
+
+    // 의존관계를 외부에서 주입당함 -> DI
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
